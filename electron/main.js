ca32540c5ad213d84557149b904f41f9aff2f670
Vitaly <vkryloveczkij@gmail.com>
2022-01-16 20:40:07 +0300

    added mouse action

--- electron/main.js
+++ electron/main.js
@@ -1,9 +1,17 @@
 // Модули для управления приложением и создания окна
-const {app, BrowserWindow} = require('electron')
+const {app, 
+  BrowserWindow, 
+  ipcMain, 
+  Menu, 
+  MenuItem, 
+  Notification, 
+  globalShortcut, 
+  Tray} = require('electron')
 const path = require ('path');
 const url = require ('url');
-//require('./ipcmain');
 var robot = require("robotjs");
+
+let tray = null
 
 function createWindow () {
   // Создаем окно браузера.
@@ -11,9 +19,11 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
+
 
   // и загрузить index.html приложения.
   //mainWindow.loadFile('index.html')
@@ -30,19 +40,50 @@ function createWindow () {
   // Отображаем средства разработчика.
   // mainWindow.webContents.openDevTools()
 }
+ipcMain.on('start', (_, data) => {
 
+  const max = 500
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max)
+  }  
+  // const width = getRandomInt(max)
+  // const height = getRandomInt(max)
+    setTimeout( function run() {
+      let action = getRandomInt(3)
+      if(action === 0) { 
+        robot.moveMouseSmooth(getRandomInt(max), getRandomInt(max))
+      } else if(action === 1) {
+        robot.mouseClick()
+      } else if (action === 2) { 
+        robot.scrollMouse(50, 0) 
+      }
+      setTimeout(run, 2000)
+      console.log(action);
+    }, 0)
+
+})
+
+function showNotification () {
+  new Notification({ title: 'Electron', body: 'Приложение запущено' }).show()
+}
 // Этот метод вызывается когда приложение инициализируется
 // и будет готово для создания окон.
 // Некоторые API могут использоваться только после возникновения этого события.
 app.whenReady().then(() => {
   createWindow()
-  robot.moveMouse(100, 100)
-  
-  app.on('activate', function () {
-    // На MacOS обычно пересоздают окно в приложении,
-    // после того, как на иконку в доке нажали и других открытых окон нету.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  showNotification()
+  tray = new Tray('/electron/tray.png')
+  const size = robot.screen.capture([x], [y], [width], [height])
+  console.log(size.width);
+})
+// .then(() => {
+//   globalShortcut.register('Alt+CommandOrControl+I', () => {
+//     console.log('Electron loves global shortcuts!')
+//   })
+app.on('activate', function () {
+  // На MacOS обычно пересоздают окно в приложении,
+  // после того, как на иконку в доке нажали и других открытых окон нету.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // Выйти когда все окна закрыты
@@ -53,4 +94,4 @@ app.on('window-all-closed', function () {
 })
 
 // В этом файле вы можете включить остальную часть основного процесса вашего приложения
-//  Вы также можете поместить их в отдельные файлы и подключить через require.
+// Вы также можете поместить их в отдельные файлы и подключить через require.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-//const electron = window.require('electron');
-//const ipcRenderer  = electron.ipcRenderer;
+const electron = window.require('electron');
+const ipcRenderer  = electron.ipcRenderer;
 
 function App() {
+  const onStart = () => {
+    ipcRenderer.send('start',  true)
+  }
+  const onStop = () => {
+    ipcRenderer.send('start', false)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onStart}>Start</button>
+      <button onClick={onStop}>Stop</button>
     </div>
   );
 }
